1233656305db99e2f5b7944316dd84f2833b97e1
eric <eric355402@yahoo.com>
2020-05-26 00:11:02 -0500

    grabbed original navbar from psd, unable to grab image, navbar mostly complete

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "../images/IFIT.png";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="container">
+      <header>
+        <img src={logo} alt="logo" />
+        <nav>
+          <a href="#">
+            <h6>Login</h6>
+          </a>
+          <a href="about.html">
+            <h6>Signup</h6>
+          </a>
+          <a href="#">
+            <h6>Menu</h6>
+          </a>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
