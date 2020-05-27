3c0b7108af03634ad86d5f8ca8c1bc9d40cb025e
eric <eric355402@yahoo.com>
2020-05-26 19:13:41 -0500

    replaced middleContent images with pexel images

--- src/components/middleContent/MiddleContent.js
+++ src/components/middleContent/MiddleContent.js
@@ -1,7 +1,18 @@
 import React from "react";
 import "./MiddleContent.css";
+
 import woman from "../../images/woman-girl-silhouette-jogger-40751.jpg";
 import weights from "../../images/weight.jpg";
+import barbell from "../../images/barbell.jpg";
+import workout from "../../images/man-working-out-2294361.jpg";
+import running from "../../images/people-wearing-running-shoes-2526878.jpg";
+import twoWoman from "../../images/two-woman-doing-exercise-1671217.jpg";
+import cityExcercise from "../../images/cityExcersice.jpg";
+import earphones from "../../images/earphones.jpg";
+import stretch from "../../images/stretch.jpg";
+import woods from "../../images/woods.jpg";
+import insideGym from "../../images/insideGym.jpg";
+import colorful from "../../images/colorful.jpg";
 
 const MiddleContent = () => {
   return (
@@ -23,34 +34,34 @@ const MiddleContent = () => {
           <img src={weights} alt="weights" />
         </div>
         <div className="box cadetblue">
-          <img src={woman} alt="woman" />
+          <img src={barbell} alt="barbell" />
         </div>
         <div className="box coral">
-          <img src={weights} alt="weights" />
+          <img src={workout} alt="workout" />
         </div>
         <div className="box crimson">
-          <img src={woman} alt="woman" />
+          <img src={running} alt="woman" />
         </div>
         <div className="box forestgreen">
-          <img src={weights} alt="weights" />
+          <img src={cityExcercise} alt="city excercise" />
         </div>
         <div className="box darkorchid">
-          <img src={woman} alt="woman" />
+          <img src={twoWoman} alt="woman" />
         </div>
         <div className="box hotpink">
-          <img src={weights} alt="weights" />
+          <img src={earphones} alt="woman with earphones" />
         </div>
         <div className="box indigo">
-          <img src={woman} alt="woman" />
+          <img src={colorful} alt="colorful weights" />
         </div>
         <div className="box dodgerblue">
-          <img src={weights} alt="weights" />
+          <img src={woods} alt="running in the woods" />
         </div>
         <div className="box indigo">
-          <img src={woman} alt="woman" />
+          <img src={stretch} alt="woman stretching" />
         </div>
         <div className="box dodgerblue">
-          <img src={weights} alt="weights" />
+          <img src={insideGym} alt="excercise inside gym" />
         </div>
       </div>
       <button>View More</button>
